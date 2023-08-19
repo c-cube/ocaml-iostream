d65b45ec5ef1c4bd321c5bf2f4c8345fb1c1c88e
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-08-19 17:33:01 -0400

    more tests

--- test/t_in.ml
+++ test/t_in.ml
@@ -1,7 +1,7 @@
 open Iostream
 open OUnit2
 
-let t1 =
+let t_of_str =
   "in1" >:: fun _ctx ->
   let ic = In.of_string "hello world" in
   let buf = Bytes.create 4 in
@@ -14,13 +14,13 @@ let t1 =
   assert_equal 0 (In.input ic buf 0 3);
   ()
 
-let t2 =
+let t_empty =
   "in empty" >:: fun _ctx ->
   let ic = In.empty in
   let buf = Bytes.create 4 in
   assert_equal 0 (In.input ic buf 0 4)
 
-let t3 =
+let t_str_to_buf =
   "transfer string to buf" >:: fun _ctx ->
   let ic = In.of_string ~len:1_500 (String.make 2_000 'a') in
   let buf = Buffer.create 32 in
@@ -28,7 +28,7 @@ let t3 =
   assert_equal 1_500 (Buffer.length buf);
   assert_equal (String.make 1_500 'a') (Buffer.contents buf)
 
-let t4 =
+let t_close_str =
   "close in string midway" >:: fun _ctx ->
   let ic = In.of_string (String.make 2_000 'a') in
   let buf = Bytes.create 100 in
@@ -40,7 +40,7 @@ let t4 =
   let n = In.input ic buf 0 100 in
   assert_equal ~msg:"must be empty after close" 0 n
 
-let t5 =
+let t_big_read =
   "big read" >:: fun _ctx ->
   let ic = In.of_string (String.make 2_000 'a') in
   let buf = Bytes.create 10_000 in
@@ -49,5 +49,36 @@ let t5 =
   let n = In.input ic buf 0 10_000 in
   assert_equal 0 n
 
-let suite = "in" >::: [ t1; t2; t3; t4; t5 ]
+let t_concat =
+  "concat" >:: fun _ctx ->
+  let i =
+    In.concat
+      [
+        In.of_string "hello";
+        In.empty;
+        In.of_string " ";
+        In.of_string "world";
+        In.of_string "!";
+      ]
+  in
+  let r = In_buf.of_in i |> In_buf.input_all in
+  assert_equal "hello world!" r
+
+let t_map =
+  "map" >:: fun _ctx ->
+  let i = In.of_string "hello world!" |> In.map_char Char.uppercase_ascii in
+  assert_equal "HELLO WORLD!" (In_buf.of_in i |> In_buf.input_all)
+
+let suite =
+  "in"
+  >::: [
+         t_of_str;
+         t_empty;
+         t_str_to_buf;
+         t_close_str;
+         t_big_read;
+         t_concat;
+         t_map;
+       ]
+
 let () = OUnit2.run_test_tt_main suite

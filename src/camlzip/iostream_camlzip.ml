5c0e847e14f8500bc3ea92a90011b5e6d9cbef5e
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-02-26 15:10:00 -0500

    camlzip: add buffered version of the input stream transducers

--- src/camlzip/iostream_camlzip.ml
+++ src/camlzip/iostream_camlzip.ml
@@ -2,6 +2,7 @@ open Iostream
 
 open struct
   let default_buf_size = 16 * 1024
+  let _default_comp_level = 4
 
   let get_buf ?buf_size ?buf () =
     match buf with
@@ -20,7 +21,7 @@ type mode =
   | Inflate
   | Deflate of int
 
-let transduce_in_ ~mode (ic : #In_buf.t) : In.t =
+class transduce_in_ ~mode (ic : #In_buf.t) : In.t =
   let zlib_str =
     match mode with
     | Inflate -> Zlib.inflate_init false
@@ -74,10 +75,38 @@ let transduce_in_ ~mode (ic : #In_buf.t) : In.t =
   end
 
 let[@inline] decompress_in (ic : #In_buf.t) : In.t =
-  transduce_in_ ~mode:Inflate ic
+  new transduce_in_ ~mode:Inflate ic
 
-let[@inline] compress_in ?(level = 4) (ic : #In_buf.t) : In.t =
-  transduce_in_ ~mode:(Deflate level) ic
+let[@inline] compress_in ?(level = _default_comp_level) (ic : #In_buf.t) : In.t
+    =
+  new transduce_in_ ~mode:(Deflate level) ic
+
+let decompress_in_buf ?buf_size ?buf (ic : #In_buf.t) : In_buf.t =
+  let bytes = get_buf ?buf_size ?buf () in
+  object
+    (* use [transduce_in_] but hide its [input] method *)
+    inherit transduce_in_ ~mode:Inflate ic as underlying
+
+    (* use regular bufferized [input] *)
+    inherit! In_buf.t_from_refill ~bytes ()
+
+    method private refill (slice : Slice.t) =
+      slice.len <- underlying#input slice.bytes 0 (Bytes.length slice.bytes)
+  end
+
+let compress_in_buf ?buf_size ?buf ?(level = _default_comp_level)
+    (ic : #In_buf.t) : In_buf.t =
+  let bytes = get_buf ?buf_size ?buf () in
+  object
+    (* use [transduce_in_] but hide its [input] method *)
+    inherit transduce_in_ ~mode:(Deflate level) ic as underlying
+
+    (* use regular bufferized [input] *)
+    inherit! In_buf.t_from_refill ~bytes ()
+
+    method private refill (slice : Slice.t) =
+      slice.len <- underlying#input slice.bytes 0 (Bytes.length slice.bytes)
+  end
 
 (* write output buffer to out *)
 let write_out (oc : Out.t) (slice : Slice.t) : unit =
@@ -87,9 +116,10 @@ let write_out (oc : Out.t) (slice : Slice.t) : unit =
     slice.len <- 0
   )
 
-let transduce_out_ ?buf_size ?buf ~mode (oc : Out.t) : Out.t =
-  let buf = get_buf ?buf_size ?buf () in
-  let slice = Slice.of_bytes buf in
+let transduce_out_ ?buf_size ?buf ~mode (oc : Out.t) : Out_buf.t =
+  let b1 = Bytes.create 1 in
+  let bytes = get_buf ?buf_size ?buf () in
+  let slice = Slice.of_bytes bytes in
   let zlib_str =
     match mode with
     | Inflate -> Zlib.inflate_init false
@@ -140,11 +170,17 @@ let transduce_out_ ?buf_size ?buf ~mode (oc : Out.t) : Out.t =
       | Deflate _ -> Zlib.deflate_end zlib_str);
       Out.close oc
 
+    method output_char c =
+      Bytes.set b1 0 c;
+      write_zlib ~flush:Zlib.Z_NO_FLUSH oc b1 0 1
+
     method output buf i len = write_zlib ~flush:Zlib.Z_NO_FLUSH oc buf i len
+    method flush () = flush_zlib ~flush:Zlib.Z_FULL_FLUSH oc
   end
 
-let compress_out ?buf_size ?buf ?(level = 4) oc : Out.t =
+let compressed_out ?buf_size ?buf ?(level = _default_comp_level) oc : Out_buf.t
+    =
   transduce_out_ ?buf_size ?buf ~mode:(Deflate level) oc
 
-let decompress_out ?buf_size ?buf oc : Out.t =
+let decompressed_out ?buf_size ?buf oc : Out_buf.t =
   transduce_out_ ?buf_size ?buf ~mode:Inflate oc
--- src/camlzip/iostream_camlzip.mli
+++ src/camlzip/iostream_camlzip.mli
@@ -1,6 +1,29 @@
 open Iostream
 
 val decompress_in : In_buf.t -> In.t
+(** [decompress_in ic] returns a new input stream
+    that is the decompressed version of [ic] *)
+
+val decompress_in_buf : ?buf_size:int -> ?buf:bytes -> In_buf.t -> In_buf.t
+(** Like {!decompress_in} but the output is buffered as well. *)
+
 val compress_in : ?level:int -> In_buf.t -> In.t
-val compress_out : ?buf_size:int -> ?buf:bytes -> ?level:int -> Out.t -> Out.t
-val decompress_out : ?buf_size:int -> ?buf:bytes -> Out.t -> Out.t
+(** [compress_in ?level ic] is a new input stream
+    that is the compressed version of [ic].
+    @param level optional Zlib compression level *)
+
+val compress_in_buf :
+  ?buf_size:int -> ?buf:bytes -> ?level:int -> In_buf.t -> In_buf.t
+(** Same as {!compress_in} but returning a buffered input. *)
+
+val compressed_out :
+  ?buf_size:int -> ?buf:bytes -> ?level:int -> Out.t -> Out_buf.t
+(** [compressed_out oc] takes a output stream [oc], and
+    returns a new output stream [oc2]. Writing some (normal) bytes to [oc2]
+    will write a compressed version of these bytes into [oc] (possibly
+    after a flush). *)
+
+val decompressed_out : ?buf_size:int -> ?buf:bytes -> Out.t -> Out_buf.t
+(** [decompressed_out oc] is a new output stream [oc2]. Writing
+    (compressed) bytes to [oc2] will write their decompressed version
+    into [oc] (possibly after a flush) *)

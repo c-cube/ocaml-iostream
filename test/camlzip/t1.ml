c67796ea258cb70565878f4b78e31fb69831efde
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-02-26 00:21:47 -0500

    more test

--- test/camlzip/t1.ml
+++ test/camlzip/t1.ml
@@ -1,7 +1,7 @@
 module IO = Iostream
 module IOZ = Iostream_camlzip
 
-let t_decompress_of_compress str =
+let t_decompress_oc_of_compress_ic str =
   let ic = IO.In_buf.of_string str in
   let buf = Buffer.create 32 in
   let oc = IO.Out.of_buffer buf in
@@ -10,10 +10,40 @@ let t_decompress_of_compress str =
   if str <> str' then ();
   ()
 
-let () = t_decompress_of_compress "hello world"
+let t_decompress_ic_of_compress_ic str =
+  let ic = IO.In_buf.of_string str in
+  let buf = Buffer.create 32 in
+  let oc = IO.Out.of_buffer buf in
+  IO.In.copy_into
+    (IOZ.decompress_in @@ IO.In_buf.bufferized @@ IOZ.compress_in ic)
+    oc;
+  let str' = Buffer.contents buf in
+  if str <> str' then ();
+  ()
+
+let t_decompress_oc_of_compress_oc str =
+  let ic = IO.In_buf.of_string str in
+  let buf = Buffer.create 32 in
+  let oc = IO.Out.of_buffer buf in
+  IO.In.copy_into ic (IOZ.compress_out @@ IOZ.decompress_out oc);
+  let str' = Buffer.contents buf in
+  if str <> str' then ();
+  ()
+
+let test str =
+  Printf.printf "oc of ic (len=%d)\n%!" (String.length str);
+  t_decompress_oc_of_compress_ic str;
+  Printf.printf "ic of ic (len=%d)\n%!" (String.length str);
+  t_decompress_ic_of_compress_ic str;
+  Printf.printf "oc of oc (len=%d)\n%!" (String.length str);
+  t_decompress_oc_of_compress_oc str;
+  Printf.printf "passed\n%!";
+  ()
+
+let () = test "hello world"
 
 let () =
   let s = String.init 26 (fun i -> Char.chr (Char.code 'a' + i)) in
   let l = List.init 1000 (fun _ -> s) in
   let str = String.concat "." l in
-  t_decompress_of_compress str
+  test str

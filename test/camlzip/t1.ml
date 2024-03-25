e9c8af154f624abecfccdf9b8a8650eb633ec698
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-03-25 09:55:19 -0400

    more tests

--- test/camlzip/t1.ml
+++ test/camlzip/t1.ml
@@ -10,6 +10,15 @@ let t_decompress_oc_of_compress_ic str =
   if str <> str' then ();
   ()
 
+let t_decompress_oc_buf_of_compress_ic str =
+  let ic = IO.In_buf.of_string str in
+  let buf = Buffer.create 32 in
+  let oc = IO.Out_buf.of_buffer buf in
+  IO.In.copy_into (IOZ.compress_in ic) (IOZ.decompressed_out_buf oc);
+  let str' = Buffer.contents buf in
+  if str <> str' then ();
+  ()
+
 let t_decompress_ic_of_compress_ic str =
   let ic = IO.In_buf.of_string str in
   let buf = Buffer.create 32 in
@@ -30,13 +39,37 @@ let t_decompress_oc_of_compress_oc str =
   if str <> str' then ();
   ()
 
+let t_decompress_oc_buf_of_compress_buf str =
+  let ic = IO.In_buf.of_string str in
+  let buf = Buffer.create 32 in
+  let oc = IO.Out_buf.of_buffer buf in
+  IO.In.copy_into ic (IOZ.compressed_out_buf @@ IOZ.decompressed_out oc);
+  let str' = Buffer.contents buf in
+  if str <> str' then ();
+  ()
+
+let t_decompress_oc_buf_of_compress_buf_oc str =
+  let ic = IO.In_buf.of_string str in
+  let buf = Buffer.create 32 in
+  let oc = IO.Out_buf.of_buffer buf in
+  IO.In.copy_into ic (IOZ.compressed_out_buf @@ IOZ.decompressed_out_buf oc);
+  let str' = Buffer.contents buf in
+  if str <> str' then ();
+  ()
+
 let test str =
   Printf.printf "oc of ic (len=%d)\n%!" (String.length str);
   t_decompress_oc_of_compress_ic str;
+  Printf.printf "oc_buf of ic (len=%d)\n%!" (String.length str);
+  t_decompress_oc_buf_of_compress_ic str;
   Printf.printf "ic of ic (len=%d)\n%!" (String.length str);
   t_decompress_ic_of_compress_ic str;
   Printf.printf "oc of oc (len=%d)\n%!" (String.length str);
   t_decompress_oc_of_compress_oc str;
+  Printf.printf "oc_buf of oc (len=%d)\n%!" (String.length str);
+  t_decompress_oc_buf_of_compress_buf str;
+  Printf.printf "oc_buf of oc_buf (len=%d)\n%!" (String.length str);
+  t_decompress_oc_buf_of_compress_buf_oc str;
   Printf.printf "passed\n%!";
   ()
 
